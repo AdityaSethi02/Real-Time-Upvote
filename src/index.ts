3f87ecf1ffcb5ada56d608946a411f96dc5afc24
Aditya Sethi <adityasethi2004@gmail.com>
2024-10-08 00:29:55 +0530

    checkpoints

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+
+import {server as WebSocketServer} from "websocket";
+import http from "http";
+
+var server = http.createServer(function(request: any, response: any) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin: string) {
+    return true;
+}
+
+wsServer.on('request', function(request: any) {
+    if (!originIsAllowed(request.origin)) {
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message: any) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode: number, description: string) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
