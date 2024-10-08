b580eb3344989da710a9d06b11c55c5de69d6e34
Aditya Sethi <adityasethi2004@gmail.com>
2024-10-08 18:31:53 +0530

    added init msg handlers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 
-import {server as WebSocketServer} from "websocket";
+import {OutgoingMessage, SupportedMessage as OutgoingSupportedMessages} from "./messages/outgoingMessages";
+import {connection, server as WebSocketServer} from "websocket";
 import http from "http";
+import { IncomingMessage, SupportedMessage } from "./messages/incomingMessages";
+import { UserManager } from "./UserManager";
+import { InMemoryStore } from "./store/InMemoryStore";
 
-var server = http.createServer(function(request: any, response: any) {
+const server = http.createServer(function(request: any, response: any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
+
+const userManager = new UserManager();
+const store = new InMemoryStore();
 
 server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
@@ -31,16 +38,71 @@ wsServer.on('request', function(request: any) {
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message: any) {
+        // Todo: add rate limiting here
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+            try {
+                messageHandler(connection, JSON.parse(message.utf8Data));
+            } catch (error) {
+
+            }
         }
     });
     connection.on('close', function(reasonCode: number, description: string) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+function messageHandler(message: IncomingMessage, ws: connection) {
+    if (message.type === SupportedMessage.JoinRoom) {
+        const payload = message.payload;
+        userManager.addUser(payload.userId, payload.name, payload.roomId, ws);
+    }
+
+    if (message.type === SupportedMessage.SendMessage) {
+        const payload = message.payload;
+        const user = userManager.getUser(payload.roomId, payload.userId);
+
+        if (!user) {
+            console.error("No user found")
+            return;
+        }
+        let chat = store.addChats(payload.userId, user.name, payload.roomId, payload.message);
+
+        if (!chat) {
+            console.error("No chat found")
+            return;
+        }
+
+        const outgoingPayload: OutgoingMessage = {
+            type: OutgoingSupportedMessages.AddChat,
+            payload: {
+                chatId: chat.id,
+                roomId: payload.roomId,
+                message: payload.message,
+                name: user.name,
+                upVotes: 0
+            }
+        }
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+    }
+
+    if (message.type === SupportedMessage.UpvoteMessage) {
+        const payload = message.payload;
+        const chat = store.upVote(payload.userId, payload.roomId, payload.chatId);
+
+        if (!chat) {
+            console.error("No chat found")
+            return;
+        }
+
+        const outgoingPayload: OutgoingMessage = {
+            type: OutgoingSupportedMessages.UpdateChat,
+            payload: {
+                chatId: payload.chatId,
+                roomId: payload.roomId,
+                upVotes: chat.upVotes.length
+            }
+        }
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+    }
+}
--- src/store/InMemoryStore.ts
+++ src/store/InMemoryStore.ts
@@ -34,18 +34,21 @@ export class InMemoryStore implements Store {
         const room = this.store.get(roomId);
 
         if (!room) {
-            return;
+            return null;
         }
 
-        room.chats.push({
+        const chat = {
             id: (globalChatId++).toString(),
             userId,
             name,
             message,
             upVotes: []
+        }
+
+        room.chats.push(chat);
+        return chat;
 
 
-        });
     }
 
     upVote (userId: UserId, roomId: string, chatId: string) {
@@ -60,5 +63,7 @@ export class InMemoryStore implements Store {
         if (chat) {
             chat.upVotes.push(userId);
         }
+
+        return chat;
     }
 }
